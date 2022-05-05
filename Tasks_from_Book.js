3708980c39a97062f9ca1a4e2ed3a972b749804b
Shparky2022 <igorozer103409@gmail.com>
2022-05-05 22:06:04 +0300

    file create

---
+++ Tasks_from_Book.js
@@ -0,0 +1,14 @@
+age = function(age) {
+
+if (age >= 18){
+    console.log('вам разрешено посещение сайта')
+} else {
+    console.log('вам запрещено посещение сайта')
+}
+
+}
+
+age(30)
+age(18)
+age(17)
+age(21)
